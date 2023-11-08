65a87d4f85a6eb505a74d5228c6509cf0cc64f1e
Peter <peterr.00000@gmail.com>
2023-11-09 04:51:26 +0800

    initial commit - working

---
+++ README.md
@@ -0,0 +1,29 @@
+## Icons
+
+# Rich presence for LTspice
+
+## Install and run
+
+Install dependencies:
+
+```bash
+python -m venv .venv
+source .venv/bin/activate
+python -m pip install -r requirements.txt
+```
+
+Run with:
+
+```bash
+python .\ltspice-discord.py
+```
+
+## Icons
+
+Icons adapted from:
+
+https://commons.wikimedia.org/wiki/File:BJT_NPN_symbol-fr.svg
+
+https://en.wikipedia.org/wiki/File:Oscillation_amortie.svg?&useskin=vector
+
+https://commons.wikimedia.org/wiki/File:AND_ANSI.svg
---
+++ ltspice-discord.py
@@ -0,0 +1,136 @@
+import os
+from typing import List
+import win32gui
+import time
+import psutil
+import win32process
+import pypresence
+
+rpc = pypresence.Presence("1171889899679531129")
+data = None
+
+rpc.connect()
+
+
+def find_active_processes(name: str) -> List[int]:
+    processes = []
+    for pid in psutil.pids():
+        try:
+            if psutil.Process(pid) and psutil.Process(pid).name() == name:
+                processes.append(pid)
+        except psutil.NoSuchProcess:
+            pass
+    return processes
+
+
+LTSPICE_NAME = "LTSPICE.exe"
+
+last_state = {}
+last_program = {"pid": None, "path": None}
+
+
+def handle_ltspice(title: str) -> None:
+    global last_state
+    filename = title.replace("LTspice XVII - ", "")
+    file, ext = os.path.splitext(filename)
+    state = {}
+    if ext in (".asc",):  # SCHEMATICS
+        state = {
+            "large_image": "schematic",
+            "large_text": "Schematic",
+            "details": "Editing a schematic",
+            "state": filename,
+        }
+    elif ext in (
+        ".cir",
+        ".net",
+        ".sp",
+    ):  # NETLISTS
+        state = {
+            "large_image": "symbol",  # TODO: Add images for these
+            "large_text": "Symbol",
+            "details": "Editing a netlist",
+            "state": filename,
+        }
+    elif ext in (
+        ".raw",
+        ".fra",
+    ):  # WAVEFORMS
+        state = {
+            "large_image": "graph",
+            "large_text": "Waveform",
+            "details": "Viewing a graph",
+            "state": filename,
+        }
+    elif ext in (
+        ".bjt",
+        ".cap",
+        ".dio",
+        ".ind",
+        ".jft",
+        ".mos",
+        ".res",
+    ):  # DISCRETES
+        state = {
+            "large_image": "symbol",  # TODO: Add images for each one of these
+            "large_text": "Symbol",
+            "details": "Editing a discrete component",
+            "state": filename,
+        }
+    elif ext in (".asy",):  # SYMBOLS
+        state = {
+            "large_image": "symbol",
+            "large_text": "Symbol",
+            "details": "Editing a symbol",
+            "state": filename,
+        }
+    else:
+        rpc.clear()
+        state = {
+            "large_image": "logo",
+            "large_text": "LTspice",
+            "details": "Idling",
+        }
+
+    if state != last_state:
+        print("UPDATED!")
+        print(filename, file, ext)
+        rpc.update(
+            **state,
+            start=int(time.time()),
+            small_image="logo",
+            small_text="LTspice",
+        )
+        last_state = state
+        return True
+    return False
+
+
+def main() -> None:
+    global last_program
+    WINDOW_HANDLERS = {
+        LTSPICE_NAME: handle_ltspice,
+    }
+    while True:
+        time.sleep(3)
+        foreground_hwnd = win32gui.GetForegroundWindow()
+        pid = win32process.GetWindowThreadProcessId(foreground_hwnd)
+        if pid[-1] >= 0:
+            active_process = psutil.Process(pid[-1])
+            if active_process.name() in WINDOW_HANDLERS.keys():
+                handler = WINDOW_HANDLERS[active_process.name()]
+                if handler(win32gui.GetWindowText(foreground_hwnd)):
+                    last_program = {
+                        "pid": active_process.pid,
+                        "path": active_process.name(),
+                    }
+        if last_program["pid"] is not None and last_program[
+            "pid"
+        ] not in find_active_processes(last_program["path"]):
+            last_program = {"pid": None, "path": None}
+            print("Clear RPC - program closed")
+            rpc.clear()
+
+
+if __name__ == "__main__":
+    main()
